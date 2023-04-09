e00e8e43a40827747b57315ac8b365fe4cff6c8d
qquileh <94903497+qquileh@users.noreply.github.com>
2023-04-10 02:25:59 +0300

    Added losing when eating the snake

--- game.py
+++ game.py
@@ -18,43 +18,90 @@ class Game:
         self.apple = Apple(self.screen)
         self.apple.draw()
 
+    def reset(self):
+        self.snake = Snake(self.screen, 1)
+        self.apple = Apple(self.screen)
+
+    def show_score(self):
+        font = pygame.font.SysFont('calibri', 20)
+        score = font.render(f"Счёт: {self.snake.length}", True, (0, 0, 0))
+        self.screen.blit(score, (15, 15))
+
+    def eat_apple(self):
+        if self.is_contact(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def eat_snake(self):
+        for i in range(3, self.snake.length):
+            if self.is_contact(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Game over"
+
+
+    def game_on(self):
+        self.snake.go()
+        self.apple.draw()
+        self.show_score()
+        pygame.display.update()
+        self.eat_apple()
+        self.eat_snake()
 
     def run(self):
         running = True
-
+        pause = False
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
-                    if event.key == K_q:
+                    if event.key == K_SPACE:
+                        pause = False
+
+                    elif event.key == K_q:
                         running = False
 
-                    if event.key == K_UP:
-                        self.snake.move_up()
+                    elif event.key == K_UP:
+                        if self.snake.direction == "left" or self.snake.direction == "right":
+                            self.snake.move_up()
 
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
+                    elif event.key == K_DOWN:
+                        if self.snake.direction == "left" or self.snake.direction == "right":
+                            self.snake.move_down()
 
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
+                    elif event.key == K_LEFT:
+                        if self.snake.direction == "up" or self.snake.direction == "down":
+                            self.snake.move_left()
 
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
+                    elif event.key == K_RIGHT:
+                        if self.snake.direction == "up" or self.snake.direction == "down":
+                            self.snake.move_right()
 
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.go()
-            self.apple.draw()
-            if self.is_eating_apple(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
-                self.snake.increase_length()
-                self.apple.move()
+            try:
+                if not pause:
+                    self.game_on()
 
-            time.sleep(0.2)
+            except Exception:
+                self.game_over()
+                pause = True
+                self.reset()
 
-    def is_eating_apple(self, x_1, y_1, x_2, y_2):
-        if x_1 >= x_2 and x_1 < x_2 + SQUARE:
-            if y_1 >= y_2 and y_1 < y_2 + SQUARE:
+            time.sleep(0.1)
+        else:
+            self.game_over()
+
+
+    def is_contact(self, x_1, y_1, x_2, y_2):
+        if x_1 == x_2:
+            if y_1 == y_2:
                 return True
         return False
 
-
+    def game_over(self):
+        self.screen.fill((10, 200, 50))
+        font = pygame.font.SysFont('calibri', 30)
+        game_over = font.render(f"Игра окончена. Ваш счёт: {self.snake.length}", True, (0, 0, 0))
+        self.screen.blit(game_over, (300, 400))
+        play_again = font.render("Нажмите 'пробел', чтобы играть снова", True, (0, 0, 0))
+        self.screen.blit(play_again, (300, 440))
+        pygame.display.update()
