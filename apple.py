f08bc2b1f1e12fb37ad9f88de0561c641171f457
qquileh <94903497+qquileh@users.noreply.github.com>
2023-04-25 18:49:43 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -2,19 +2,20 @@ import pygame
 import random
 
 SQUARE = 40
+NUM_OF_SQUARES = 20
+
 
 class Apple:
     def __init__(self, screen):
         self.body = pygame.image.load("images/apple.jpg").convert()
         self.screen = screen
-        self.x = random.randint(0, 19) * SQUARE
-        self.y = random.randint(0, 19) * SQUARE
+        self.x = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
+        self.y = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
 
     def draw(self):
         self.screen.blit(self.body, (self.x, self.y))
         pygame.display.update()
 
     def move(self):
-        self.x = random.randint(0, 19) * SQUARE
-        self.y = random.randint(0, 19) * SQUARE
-
+        self.x = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
+        self.y = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
