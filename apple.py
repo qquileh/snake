cd144044bd3671ef3f4df68913872ac05a1612e7
qquileh <94903497+qquileh@users.noreply.github.com>
2023-04-25 22:40:00 +0300

    выбор уровней

--- apple.py
+++ apple.py
@@ -2,11 +2,11 @@ import pygame
 import random
 
 SQUARE = 40
-NUM_OF_SQUARES = 20
 
 
 class Apple:
-    def __init__(self, screen):
+    def __init__(self, screen, NUM_OF_SQUARES):
+        self.NUM_OF_SQUARES = NUM_OF_SQUARES
         self.body = pygame.image.load("images/apple.jpg").convert()
         self.screen = screen
         self.x = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
@@ -17,5 +17,5 @@ class Apple:
         pygame.display.update()
 
     def move(self):
-        self.x = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
-        self.y = random.randint(0, NUM_OF_SQUARES - 1) * SQUARE
+        self.x = random.randint(0, self.NUM_OF_SQUARES - 1) * SQUARE
+        self.y = random.randint(0, self.NUM_OF_SQUARES - 1) * SQUARE
